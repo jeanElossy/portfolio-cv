47da4b379f3ad8765f7b8ab176fdb62f3cc9a0aa
JeanElossy <jean.elossy@yahoo.com>
2021-10-04 00:44:01 +0100

    initialisation du projet

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const Contat = () => {
     return (
@@ -9,7 +10,66 @@ const Contat = () => {
                 <NavBar />
             </div>
             <div className="contactContent col-12 col-md-8 text-white">
-                contenu contact
+                
+                <div className="header"></div>
+
+                <div className="contactBox">
+                    <h1 className="text-center mb-5">Contactez-moi</h1>
+                    <ul>
+                        <li>
+                            <i className="fas fa-map-marker-alt"></i>
+                            <span>Abidjan / Cote d'Ivoire</span> 
+                        </li>
+
+                        <li>
+                            <i className="fas fa-mobile-alt"></i>
+                            <CopyToClipboard text="0749490835">
+                                <span className="clickInput" onClick={() => {
+                                        alert("Numero de telephone copie !");
+                                    }}>
+                                        07 494 908 35
+                                </span>
+                            </CopyToClipboard>
+                        </li>
+
+                        <li>
+                            <i className="far fa-envelope"></i>
+                            <CopyToClipboard text="jean.elossy@gmail.com">
+                                <span className="clickInput" onClick={() => {
+                                        alert("Email copie !");
+                                    }}>
+                                        jean.elossy@gmail.com
+                                </span>
+                            </CopyToClipboard>
+                        </li>
+
+                    </ul>
+                </div>
+                <div className="socialNetwork">
+                    <ul>
+                        <a href="#" 
+                            target="_blank" 
+                            rel="noopener moreferrer"
+                        >
+                            <h4>LinkedIn</h4>
+                            <i className="fab fa-linkedin"></i>
+                        </a>
+                        <a href="#" 
+                            target="_blank" 
+                            rel="noopener moreferrer"
+                        >
+                            <h4>GitHub</h4>
+                            <i className="fab fa-github"></i>
+                        </a>
+                        <a href="#" 
+                            target="_blank" 
+                            rel="noopener moreferrer"
+                        >
+                            <h4>Twitter</h4>
+                            <i className="fab fa-twitter"></i>
+                        </a>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
@@ -18,6 +78,3 @@ const Contat = () => {
 
 export default Contat
 
-
-
-// _knowledges.scss
